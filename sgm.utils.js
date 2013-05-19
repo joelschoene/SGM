d86273f55d22082786f211cf00dad6d2e3592276
Kanakaraj Venkataswamy <kvenkataswamy@sapient.com>
2013-05-19 23:40:50 +0530

    Extending JS methods
    From Douglas Crockford - Page number 32, 33

--- sgm.utils.js
+++ sgm.utils.js
@@ -1,4 +1,15 @@
+// todo write proper comments
 var SGM = window.SGM || {};
+
+// Augmenting Types
+Function.prototype.method = function(name, func) {
+	this.prototype[name] = func;
+	return this;
+}
+// document.writeln( (-10 / 3).integer())  // -3
+String.method('integer', function() {
+	return Math[ this < 0 ? 'ceiling' : 'floor'](this);
+});
 
 SGM.Utils = (function(){
 
@@ -7,9 +18,9 @@ SGM.Utils = (function(){
 	return {
 
 		init: function() {
-			
-		}
 
+		}
+		
 	};  // return
 
 })();  // SGM.Utils
